1b690e6701f761a8bf70c5bc4b14493e50b2da7f
Michael Monteblanco <michael.thomas.monteblanco@hotmail.com>
2024-06-21 17:51:40 -0700

    campsiteSlicee

---
+++ src/app/shared/CAMPSITES.js
@@ -0,0 +1,35 @@
+import reactLakeImg from '../assets/img/react-lake.jpg';
+import chromeRiverImg from '../assets/img/chrome-river.jpg';
+import breadCrumbImg from '../assets/img/breadcrumb-trail.jpg';
+import reduxWoodsImg from '../assets/img/redux-woods.jpg';
+
+export const CAMPSITES = [
+    {
+        id: 0,
+        name: 'React Lake Campground',
+        image: reactLakeImg,
+        elevation: 1233,
+        description: 'A beautiful place to camp.'
+    },
+    {
+        id: 1,
+        name: 'Chrome River Campground',
+        image: chromeRiverImg,
+        elevation: 877,
+        description: 'A nice place with a flowing river.'
+    },
+    {
+        id: 2,
+        name: 'Breadcrumb Trail Campground',
+        image: breadCrumbImg,
+        elevation: 2901,
+        description: 'A scenic trail and camping area.'
+    },
+    {
+        id: 3,
+        name: 'Redux Woods Campground',
+        image: reduxWoodsImg,
+        elevation: 42,
+        description: 'A dense and tranquil forest.'
+    }
+];
