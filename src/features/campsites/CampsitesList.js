d0f8821ad014b339553df62042182dc4765383ee
Michael Monteblanco <michael.thomas.monteblanco@hotmail.com>
2024-06-22 20:00:48 -0700

    useState

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,19 +1,22 @@
+import React from 'react';
 import { Col, Row, } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from './campsitesSlice';
 
+const CampsitesList = ( {setCampsiteId }) => {
+    const campsites = selectAllCampsites();
 
-const CampsitesList = () => {
-  const campsites = selectAllCampsites(); 
-  return (
-    <Row className="ms-auto">
-      {CAMPSITES.map(campsite => (
-        <Col md="5" className="m-4" key={campsite.id}>
-          <CampsiteCard campsite={campsite} />
-        </Col>
-      ))}
-    </Row>
-  );
+    return (     
+        <Row className="ms-auto">
+            {campsites.map((campsite) => {
+                return (
+                    <Col md="5" className="m-4" key={campsite.id}  onClick={() => setCampsiteId(campsite.id)}>
+                        <CampsiteCard campsite={campsite} />
+                    </Col>
+                );
+            })}
+        </Row>  
+    );
 };
 
 export default CampsitesList;
